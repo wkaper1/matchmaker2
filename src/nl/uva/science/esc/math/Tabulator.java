a127f6af57142589a978a9bdb2a227f3ac0205f0
Wolter Kaper <w.h.kaper@uva.nl>
2023-03-06 19:44:42 +0100

    Plan for Tabulator - it's planned to use reflection!

--- src/nl/uva/science/esc/math/Tabulator.java
+++ src/nl/uva/science/esc/math/Tabulator.java
@@ -2,11 +2,17 @@ package nl.uva.science.esc.math;
 
 import java.util.function.DoubleUnaryOperator;
 
+/**
+ * Tabulate functions of one or more variables, given as operators, static methods, instance methods
+ * The values of the independent variable(s) to tabulate are given as double[] arrays
+ * @author Wolter2
+ */
 public class Tabulator {
 	
 	/**
-	 * Tabulate the given function at the given values of the independent variable
-	 * @param function, the function top tabulate
+	 * Tabulate the given unary function at the given values of the independent variable.
+	 * The function is given as a java "Operator", a DoubleUnaryOperator specifically.
+	 * @param function, the function to tabulate
 	 * @param points, an array of values of the independent variable, points on the x-axis
 	 */
 	public static void tabulate(DoubleUnaryOperator function, double[] points) {
@@ -20,4 +26,51 @@ public class Tabulator {
 		System.out.println();
 	}
 
+	/**
+	 * Scheme for generating the combinations of variable values to tabulate
+	 * @author Wolter2
+	 */
+	public enum VariationScheme {
+		/**
+		 * Do one pass for each of the variables while keeping the others at their midpoint 
+		 * (if number of values is uneven) or close below the midpoint (if even)
+		 */
+		ONE_PASS_PER_VARIABLE_OTHERS_AT_MIDPOINT,
+		/**
+		 * Generate all possible combinations in the systematic way known from (old fashioned) television screens
+		 */
+		ALL_COMBINATIONS_ZIGZAG
+	}
+
+	/**
+	 * Load an instance method for tabulation.
+	 * Next, call addVariable to declare each of the variables plus the values of that variable to tabulate.
+	 */
+	public Tabulator(Object obj, String methodname) {
+		//TODO
+	}
+	
+	/**
+	 * Load a static method for tabulation
+	 * Next, call addVariable to declare each of the variables plus the values of that variable to tabulate.
+	 */
+	public Tabulator(String classname, String methodname) {
+		//TODO
+	}
+	
+	/**
+	 * Declare a variable, mentioning the set of values you want to tabulate for that variable.
+	 * All variables that appear in the method should get declared.
+	 */
+	public void addVariable(String name, double[] values) {
+		//TODO
+	}
+
+	/**
+	 * Calculate and tabulate the combinations of variable values as dictated by the scheme and the given sets 
+	 * of values for each variable. Tabulate the methods return value for each of these.
+	 */
+	public void tabulate(VariationScheme scheme) {
+		//TODO
+	}
 }
